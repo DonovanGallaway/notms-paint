efbe3860c8972eef51963735d65fa9a2b2e43d80
Donovan Gallaway <donovangallaway@gmail.com>
2021-11-18 15:40:13 -0800

    working single button

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -6,80 +6,6 @@ const Canvas = (props) => {
   const contextRef = useRef(null)
   const [isDrawing, setIsDrawing] = useState(false)
 
-  const [canvasConfig, setConfig] = useState({
-    lineCap: 'round',
-    strokeStyle: 'black',
-    lineWidth: 5,
-    current: ()=>{}
-})
-
-
-  // Color Mods
-  const setContext = () => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d')
-    context.lineCap = canvasConfig.lineCap
-    context.strokeStyle = canvasConfig.strokeStyle
-    context.lineWidth = canvasConfig.lineWidth
-    contextRef.current = context;
-  }
-
-        const redState = () => {
-            setConfig({
-            lineCap: 'round',
-            strokeStyle: 'red',
-            lineWidth: 5,
-            current: redState
-        })
-            setContext()
-        }
-        const blueState = () => {
-            setConfig({
-            lineCap: 'round',
-            strokeStyle: 'blue',
-            lineWidth: 5,
-            current: blueState
-        })
-            setContext()
-        }
-        const greenState = () => {
-            setConfig({
-            lineCap: 'round',
-            strokeStyle: 'green',
-            lineWidth: 5,
-            current: greenState
-        })
-            setContext()
-        }
-        const blackState = () => {
-            setConfig({
-            lineCap: 'round',
-            strokeStyle: 'black',
-            lineWidth: 5,
-            current: blackState
-        })
-            setContext()
-        }
-        const whiteState = () => {
-            setConfig({
-            lineCap: 'round',
-            strokeStyle: 'white',
-            lineWidth: 5,
-            current: whiteState
-        })
-            setContext()
-        }
-        const grayState = () => {
-            setConfig({
-            lineCap: 'round',
-            strokeStyle: 'gray',
-            lineWidth: 5,
-            current: grayState
-        })
-            setContext()
-        }
-
-
   useEffect(()=>{
     const canvas = canvasRef.current;
     canvas.width = window.innerWidth * 2;
@@ -89,11 +15,10 @@ const Canvas = (props) => {
 
     const context = canvas.getContext('2d')
     context.scale(2,2)
-    canvasConfig.current()
-    setContext()
-    canvasConfig.current()
-    setContext()
-    
+    context.lineCap = 'round'
+    context.strokeStyle = 'black'
+    context.lineWidth = 5
+    contextRef.current = context;
   }, [])
 
   const startDrawing = ({nativeEvent}) => {
@@ -117,12 +42,6 @@ const Canvas = (props) => {
   }
   return (
       <div className='canvas'>
-        <button onClick={()=>{redState();redState()}}>Red Color</button>
-        <button onClick={()=>blueState()}>Blue Color</button>
-        <button onClick={()=>greenState()}>Green Color</button>
-        <button onClick={()=>blackState()}>Black Color</button>
-        <button onClick={()=>whiteState()}>Erase Color</button>
-        <button onClick={()=>grayState()}>Gray Color</button>
       <canvas
         onMouseDown={startDrawing}
         onMouseUp={finishDrawing}
