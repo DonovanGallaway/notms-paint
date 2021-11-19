b8d837638d5511ae7c194d0be9c7bc89f1e5a573
sarahecarter <secarter213@gmail.com>
2021-11-18 22:13:04 -0500

    initial styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Created by <a href="https://www.linkedin.com/in/donovan-gallaway/">Donovan Gallaway</a> and <a href="https://www.linkedin.com/in/scarterwebdev/">Sarah Carter</a></p>
+        </footer>
+    )
+}
+
+export default Footer
