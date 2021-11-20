5770613d744d39c9921a28fc1306133ba4110c2b
Donovan Gallaway <donovangallaway@gmail.com>
2021-11-19 21:52:38 -0800

    not registering that it should go to the side, but at least filling vertical line of 1px length

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -1,5 +1,14 @@
 import {useRef, useEffect, useState} from 'react'
 
+// For use in converting hex to rgb for fill
+// I didn't write this. I found it on Stack Overflow.
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+const hexToRgb = hex =>
+hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+          ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+  .substring(1).match(/.{2}/g)
+  .map(x => parseInt(x, 16))
+
 const Canvas = (props) => {
 
   const canvasRef = useRef(null)
@@ -7,7 +16,7 @@ const Canvas = (props) => {
   const [isDrawing, setIsDrawing] = useState(false)
   const [currentTool, setTool] = useState({tool: 'draw'})
 
-  const [penColor, setPenColor] = useState("black")
+  const [penColor, setPenColor] = useState("#000000")
   const [penWidth, setPenWidth] = useState(5)
 
   const [canvasConfig, setConfig] = useState({
@@ -69,6 +78,7 @@ const Canvas = (props) => {
     context.strokeStyle = 'black'
     context.lineWidth = 5
     contextRef.current = context;
+    clearCanvas()
   }, [])
 
   const startDrawing = ({nativeEvent}) => {
@@ -78,19 +88,104 @@ const Canvas = (props) => {
     contextRef.current.beginPath()
     contextRef.current.moveTo(offsetX, offsetY)
     } else if (currentTool.tool==='fill'){
-      ///////////////////////////////
+      
+      ///////////////////////////////////////////////////////////////////////////////////////
       // Fill Tool Testing (Dev Only)
-      ///////////////////////////////
+      ///////////////////////////////////////////////////////////////////////////////////////
+      const targetColor = hexToRgb(canvasConfig.strokeStyle) // Convert hex to rgb to interact with bitmap
+      console.log('hex color is', canvasConfig.strokeStyle)
+      console.log('targetColor is', targetColor)
+      const fillR = targetColor[0]
+      const fillG = targetColor[1]
+      const fillB = targetColor[2]
       const canvas = canvasRef.current // gets the current canvas
       const context = canvas.getContext('2d') // accesses the context object
       var imgData = context.getImageData(0,0,canvas.width, canvas.height) // calls get image data method, renders an array of every pixel represented as an array where every four indices is an rgba value of a pixel
       // console.log(imgData)
       const {offsetX, offsetY} = nativeEvent // get mouse click
       // console.log(offsetX, offsetY)
-      const pixelStack = [offsetX, offsetY] // set up an array with the starting pixel
-      console.log(pixelStack)
+      const pixelStack = [[offsetX, offsetY]] // set up an array with the starting pixel
+      
+      //////////////////////////////////////////////////////////
+      // Function to match color
+      //////////////////////////////////////////////////////////
+      const matchStartColor = (pixelPos,testPos) => // where pixelPos is the array index of the currently selected pixel
+        {
+          const startR = imgData.data[pixelPos] // Starting R value
+          const startG = imgData.data[pixelPos+1] // Starting G value
+          const startB = imgData.data[pixelPos+2] // Starting B value
+
+          // Iterates over the aray, going through the entirety of the width * 4 (due to each pixel being represented by 4 values [rgba])
+
+          // Tests if next line up is the same color
+          const r = imgData.data[testPos];	
+          const g = imgData.data[testPos+1];	
+          const b = imgData.data[testPos+2];
+          const a = imgData.data[testPos+3]
+          
+          return (r === startR && g === startG && b === startB);
+        }
+        //////////////////////////////////////////////////////////
+        // Function to change color
+        //////////////////////////////////////////////////////////
+        const colorPixel = (pixelPos) => {
+          imgData.data[pixelPos] = fillR
+          imgData.data[pixelPos+1] = fillG
+          imgData.data[pixelPos+2] = fillB
+          imgData.data[pixelPos+3] = 255
+        }
+        //////////////////////////////////////////////////////////
+        // Body of Function
+        //////////////////////////////////////////////////////////
+        
+        while(pixelStack.length) // while loop and this pop is a clever way of iterating over an unknown number of items
+        {
+          const newPos = pixelStack.pop();
+          let x = newPos[0]
+          let y = newPos[1]
+          // console.log(x,y)
+
+          let pixelPos = (y*canvas.width + x) * 4
+          while(y-- >= 0 && matchStartColor(pixelPos, pixelPos - canvas.width * 4))
+          {
+            pixelPos -= canvas.width * 4;
+          }
+          pixelPos += canvas.width * 4;
+          ++y;
+
+          let reachLeft = false;
+          let reachRight = false;
+          while (y++ < canvas.height -1 && matchStartColor(pixelPos, pixelPos + canvas.width * 4)){
+            colorPixel(pixelPos)
+            if (x>0){
+              if(matchStartColor(pixelPos, pixelPos-4)){
+                if(reachLeft === false){
+                  pixelStack.push([x-1, y])
+                  console.log(pixelStack)
+                  reachLeft = true
+                }
+              } else if (reachLeft){
+                console.log('for some reason I refuse to reach left')
+                reachLeft = false
+              }
+            }
+            if (x < canvas.width-1){
+              if(matchStartColor(pixelPos, pixelPos+4)){
+                if(!reachRight){
+                  pixelStack.push([x+1, y])
+                  reachRight = true
+                } else if (reachRight) {
+                  reachRight = false
+                }
+              }
+            }
+            pixelPos += canvas.width * 4          
+        }
+        context.putImageData(imgData, 0,0)
+
     }
   }
+}
 
   const finishDrawing = () => {
     contextRef.current.closePath()
@@ -109,19 +204,12 @@ const Canvas = (props) => {
     contextRef.current.beginPath()
     contextRef.current.moveTo(offsetX, offsetY)
   }
-  
+
   return (
       <div className='canvas'>
-        <button onClick={() => colorState("red")}>Red Color</button>
-        <button onClick={() => colorState("green")}>Green Color</button>
-        <button onClick={() => colorState("blue")}>Blue Color</button>
-        <button onClick={() => colorState("yellow")}>Yellow Color</button>
-        <button onClick={() => strokeState(20)}>Thick</button>
-        <button onClick={() => strokeState(5)}>Regular</button>
-        <button onClick={() => strokeState(2)}>Thin</button>
         <button onClick={() => toolState('fill')}>Fill Test</button>
         <button onClick={() => toolState('draw')}>Back to Draw</button>
-        <input type="color" onInput={(event) => colorState(event.target.value)}/>
+        <input type="color" onInput={(event) => {colorState(event.target.value)}}/>
         <input type="range" min="2" max="75" defaultValue="5" onChange={(event) => strokeState(event.target.value)}/>
         <button onClick={clearCanvas}>Clear</button>
       <canvas
@@ -137,4 +225,4 @@ const Canvas = (props) => {
   );
 }
 
-export default Canvas;
+export default Canvas
