a2f18ed92e0ddd075038624a858b3a84002ccd41
sarahecarter <secarter213@gmail.com>
2021-11-22 18:37:48 -0500

    updated landing page and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,31 @@
 import {Link} from 'react-router-dom'
 
 const Header = (props) => {
-    return (<>
-    <h1>Canvas App</h1>
-    <nav>
-        <Link to='/'><div>Home</div></Link>
-        <Link to='/draw'><div>App</div></Link>
-        <Link to='/community'><div>Community</div></Link>
-    </nav>
-    </>)
+
+
+    const handleClick = () => {
+        const nav = document.querySelector('nav')
+        if (nav.className === "hide") {
+            nav.className = "show";
+        }
+        else if (nav.className === "show") {
+            nav.className = "hide";
+        }
+    }
+
+    return ( 
+    <div className="header">
+        <Link to="/">
+            <h1 id="title">Scribble.</h1>
+        </Link>
+        <i className="fas fa-bars" onClick={handleClick}></i>
+        <nav className="hide">
+            <Link to='/'><div>Home</div></Link>
+            <Link to='/draw'><div>App</div></Link>
+            <Link to='/community'><div>Community</div></Link>
+        </nav>
+    </div>
+    )
 }
 
 export default Header
