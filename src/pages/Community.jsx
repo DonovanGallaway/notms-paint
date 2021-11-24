3febb4e518d1d06cd8415e77a2627b8fae853ea2
sarahecarter <secarter213@gmail.com>
2021-11-24 16:57:14 -0500

    updated community page

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import {Link} from "react-router-dom"
 
 const Community = (props) => {
 
@@ -14,6 +15,11 @@ const Community = (props) => {
 
     if (gallery && gallery.length){
         return <main className="community">
+            <h1>The Scribble Community Art Board</h1>
+            <p>Want to add your work to the board?</p>
+            <Link to="/draw">
+                    <button>Start Drawing</button>
+            </Link>
             {gallery.map((x)=> {
                 return <div key={x._id}>
                     <img src={x.dataURL}/>
